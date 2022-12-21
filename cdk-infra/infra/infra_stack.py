cdf1732e29e5d8d3477d8b205ace4d0e00de010c
David Thomas <david@authenticins.com>
2022-12-20 19:57:23 -0500

    Updating the stack

--- cdk-infra/infra/infra_stack.py
+++ cdk-infra/infra/infra_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
+    SecretValue,
     Stack,
-    aws_amplify
+    aws_amplify as amplify,
+    aws_codebuild as codebuild
 )
 from constructs import Construct
 
@@ -10,11 +12,33 @@ class InfraStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        amplify = amplify.CfnApp(
-            self, "Nextjs",
-            name="NextjsApp",
-            repository="https://github.com/david-authentic/next-cdk.git",
-            oauth_token="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",
-            build_spec="amplify.yml",
-            environment_variables=[{}]
-        )
+        amplify_app = amplify.App(self, "Nextjs",
+                                  source_code_provider=amplify.GitHubSourceCodeProvider(
+                                      owner="david-authentic",
+                                      repository="next-cdk",
+                                      oauth_token=SecretValue.secrets_manager(
+                                          "github-token")
+                                  ),
+                                  build_spec=codebuild.BuildSpec.from_object_to_yaml({
+                                      "version": "1.0",
+                                      "frontend": {
+                                          "phases": {
+                                              "preBuild": {
+                                                  "commands": ["npm ci"]
+                                              }
+                                          },
+                                          "build": {
+                                              "commands": ["npm run build"]
+                                          },
+                                      }
+                                      "artifacts": {
+                                          "baseDirectory": "out"
+                                          "files": "**/*"
+                                      }
+                                      "cache": {
+                                          "paths": {
+                                              ["node_modules/**/*"]
+                                          }
+                                      }
+                                  })
+                                  )
